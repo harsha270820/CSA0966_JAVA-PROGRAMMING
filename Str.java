39d6e0caec4ce5ec123623394bdfbb7e9ac34296
harsha270820 <122298047+harsha270820@users.noreply.github.com>
2023-01-09 22:02:19 +0530

    Add files via upload

---
+++ Str.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Str{
+public static void main(String[] args){
+String s;
+Scanner sc=new Scanner(System.in);
+s=sc.nextLine();
+System.out.println("the entered string is:"+s);
+}
+}
+
+
+
